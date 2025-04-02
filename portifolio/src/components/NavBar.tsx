63f9ea25576edddb3d1f01af3b26ac8f8bdc2282
Genisson Emilio <esgenisson@gmail.com>
2025-04-02 15:02:54 -0300

    Update

---
+++ portifolio/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return (
+        <div>
+            <ul>
+                <li>Teste</li>
+                <li>Teste2</li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar;
