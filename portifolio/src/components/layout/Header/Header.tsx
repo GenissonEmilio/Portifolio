8d524ca72cfee3009df5050180c40453ff14c136
Genisson Emilio <esgenisson@gmail.com>
2025-04-09 09:20:58 -0300

    fix menu error

--- portifolio/src/components/layout/Header/Header.tsx
+++ portifolio/src/components/layout/Header/Header.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Menu from "../MenuDesktop/Menu";
 import styles from "./Header.module.css";
-import Mobile from "../MenuMobile/Mobile";
 
 const Header: React.FC = () => {
 
     return (
         <div className={styles.menu}>
             <Menu />
-            <Mobile />
         </div>
     )
 }
--- portifolio/src/components/layout/MenuDesktop/Menu.tsx
+++ portifolio/src/components/layout/MenuDesktop/Menu.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./Menu.module.css";
+import Mobile from "../MenuMobile/Mobile";
 
 const Menu: React.FC = () => {
     const myName: string = "<Genisson Emilio/>";
@@ -10,12 +11,14 @@ const Menu: React.FC = () => {
                 <Link to="/">{myName}</Link>
             </div>
 
-            <ul className={`${styles.links}`}>
+            <ul className={styles.links}>
                 <li><Link to="/" >Home</Link></li>
                 <li><Link to="/sobre">Sobre</Link></li>
                 <li><Link to="/projetos">Projetos</Link></li>
                 <li><Link to="/contato">Contato</Link></li>
             </ul>
+
+            <Mobile />
         </nav>
     )
 }
