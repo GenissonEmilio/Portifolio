e4d8ea21e9ce9fa3aafcef3c1aaf7965c3c5a2cd
Genisson Emilio <esgenisson@gmail.com>
2025-04-13 14:58:29 -0300

    update menu mobile

--- portifolio/src/components/layout/MenuMobile/Mobile.tsx
+++ portifolio/src/components/layout/MenuMobile/Mobile.tsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa";
 import styles from "./Mobile.module.css";
+import { Link } from "react-router-dom";
 
 const Mobile: React.FC = () => {
-    //const size: number = window.innerWidth;
-    /*if (size <= 500) {
-        
-    }*/
+    const [isOpen, setOpen] = useState(false);
+    const [shouldRender, setShouldRender] = useState(false);
+
+    useEffect(() => {
+        if (isOpen) {
+            setShouldRender(true);
+        } else {
+            const timeout = setTimeout(() => setShouldRender(false), 300);
+            return () => clearTimeout(timeout);
+        }
+    }, [isOpen]);
 
     return (
         <div>
-            <button className={styles.hamburguer}>
+            <button className={styles.hamburguer} onClick={() => setOpen(!isOpen)}>
                 <FaBars />
             </button>
+
+            {shouldRender && (
+                <div className={`${styles.menu} ${isOpen? styles.menuOpen : styles.menuClose}`}>
+                    <ul>
+                        <li><Link to="/" onClick={() => setOpen(!isOpen)}>Home</Link></li>
+                        <li><Link to="/sobre" onClick={() => setOpen(!isOpen)}>Sobre</Link></li>
+                        <li><Link to="/projetos" onClick={() => setOpen(!isOpen)}>Projetos</Link></li>
+                        <li><Link to="/contato" onClick={() => setOpen(!isOpen)}>Contatos</Link></li>
+                    </ul>
+                </div>
+            )}
         </div>
     )
 }
